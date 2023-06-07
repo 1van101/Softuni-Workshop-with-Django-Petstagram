6c68b7bf4fba591f6e903668b569e1b494d3b043
IvanTsvetanov <ivan.iv.cv@gmail.com>
2023-06-07 13:47:22 +0300

    commit

--- petstagram/petstagram/pets/views.py
+++ petstagram/petstagram/pets/views.py
@@ -7,8 +7,9 @@ def add_pet(request):
     return render(request, 'pets/pet-add-page.html')
 
 
-def details_pet(request, username, slug):
-    pet = Pet.objects.get(slug=slug)
+def details_pet(request, username, pet_slug):
+    pet = Pet.objects.get(slug=pet_slug)
+    print(pet)
     all_photos = pet.photo_set.all()
     context = {
         'pet': pet,
@@ -18,9 +19,9 @@ def details_pet(request, username, slug):
     return render(request, 'pets/pet-details-page.html', context=context)
 
 
-def edit_pet(request, username, slug):
+def edit_pet(request, username, pet_slug):
     return render(request, 'pets/pet-edit-page.html')
 
 
-def delete_pet(request, username, slug):
+def delete_pet(request, username, pet_slug):
     return render(request, 'pets/pet-delete-page.html')
--- petstagram/petstagram/settings.py
+++ petstagram/petstagram/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-de6l29+4-*i616bu$lo^hxg8hx5662fi93)&-4dhga2$=0d9n$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True 
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
